657fc06fd7e50982f177ae1c2143e1b6f746a8ae
roro1303 <bitok1059@gmail.com>
2025-01-28 16:47:11 +0200

    nodes urls change

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -13,11 +13,12 @@ const publicNode = 'https://nodes.wavesnodes.com';
 const localNode = 'http://127.0.0.1:6869';
 const remoteCustomNode = "http://stage.wavescup.world:6570";
 const myNode2 = "http://stage.wavescup.world:6869";
+const myNode = "http://wavescup.world:6869";
 
 //############################################//
 // String nodeUrl = remoteCustomNode;
-String nodeUrl = publicNode;
-// String nodeUrl = myNode2;
+// String nodeUrl = publicNode;
+String nodeUrl = myNode;
 //############################################//
 
 const eggId = "C1iWsKGqLwjHUndiQ7iXpdmPum9PeCDFfyXBdJJosDRS";
